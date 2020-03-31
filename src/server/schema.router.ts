d66ceeeb80440b9c6dbf0289d64b338886d94f84
SiBell <SiBellUK@gmail.com>
2020-03-31 14:42:07 +0100

    test

--- src/server/schema.router.ts
+++ src/server/schema.router.ts
@@ -13,5 +13,9 @@ export {router as SchemaRouter};
 // https://expressjs.com/en/starter/static-files.html
 // https://expressjs.com/en/resources/middleware/serve-static.html
 
-router.use('/schemas', express.static(path.join(__dirname, '../components/deployment/schemas')));
-router.use('/schemas', express.static(path.join(__dirname, '../components/platform/schemas')));
+router.use('/schemas', (req, res): any => {
+  return res.json({value: 'hello'});
+});
+
+// router.use('/schemas', express.static(path.join(__dirname, '../components/deployment/schemas')));
+// router.use('/schemas', express.static(path.join(__dirname, '../components/platform/schemas')));
